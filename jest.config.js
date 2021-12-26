0c07bb82a14272601d255fbeb5361e6f648ef797
kula <kula@live.com>
2021-12-26 17:54:53 +0800

    feat: jest alias configure

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  moduleNameMapper:{
+    "^@/(.*)$": "<rootDir>/src/$1"
+  }
 }
