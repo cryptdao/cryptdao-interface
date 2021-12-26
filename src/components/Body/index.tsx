bc1817e7e2b003fdf05e70c6fb8cc502374ca802
kula <kula@live.com>
2021-12-27 00:47:39 +0800

    feat: show info panel

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,7 +1,14 @@
+import Info from "../Info";
+
 export default function Body() {
   return (<>
     <div className="px-0 md:px-4 max-w-[1012px] mx-auto mt-4 flex-auto">
-      body
+      <div className="hidden float-left w-1/4 lg:block">
+        <Info/>
+      </div>
+      <div className="float-right w-full pl-0 lg:w-3/4 lg:pl-5">
+        right
+      </div>
     </div>
   </>)
 }
--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -2,7 +2,25 @@
 export default function Scroller() {
 
     return (
-        <>
+      <>
+        <div className="position: fixed; width: 240px;">
+          <div className="mb-4 overflow-hidden border-t border-b rounded-none md:border md:rounded-lg bg-skin-block-bg">
+            <div className="leading-6">
+              <div className="text-center border-b bg-skin-header-bg">
+                <h3 className="mb-[2px] mx-2">
+                  CryptDAO
+                </h3>
+                <div className="mb-[12px] text-color">
+                  20.0k成员
+                </div>
+                <div className="flex justify-center gap-x-2">
+                  <button className="button px-[24px] focus-within:border-skin-link mb-4">加入</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          info
+        </div>
         </>
     )
 }
