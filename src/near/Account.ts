775a0ba1ed6d828e96667c4fe86373c36aa80dd7
kula <kula@live.com>
2021-12-28 15:28:49 +0800

    refactor: refactor near module

--- src/near/Account.ts
+++ src/near/Account.ts
@@ -1,18 +1,18 @@
-import BN from 'bn.js';
-import { baseDecode } from 'borsh';
-import * as nearAPI from 'near-api-js';
-import { ConnectedWalletAccount, Near, utils } from 'near-api-js';
+import BN from 'bn.js'
+import { baseDecode } from 'borsh'
+import * as nearAPI from 'near-api-js'
+import { ConnectedWalletAccount, Near, utils } from 'near-api-js'
 import {
   Action,
   createTransaction,
-  functionCall,
-} from 'near-api-js/lib/transaction';
-import { PublicKey } from 'near-api-js/lib/utils';
-import { createContext } from 'react';
-import { DexViewFunction } from './FT';
-import { NearFunctionCallOptions, Transaction } from './near';
+  functionCall
+} from 'near-api-js/lib/transaction'
+import { PublicKey } from 'near-api-js/lib/utils'
+import { createContext } from 'react'
+import { DexViewFunction } from './FT'
+import { NearFunctionCallOptions, Transaction } from './near'
 
-const { connect, keyStores, WalletConnection } = nearAPI;
+const { connect, keyStores, WalletConnection } = nearAPI
 
 const config = {
   networkId: 'testnet',
@@ -21,46 +21,46 @@ const config = {
   walletUrl: 'https://wallet.testnet.near.org',
   helperUrl: 'https://helper.testnet.near.org',
   explorerUrl: 'https://explorer.testnet.near.org',
-  headers: {},
-};
+  headers: {}
+}
 
-export const ACCOUNT_MIN_STORAGE_AMOUNT = '0.003';
-export const near = new Near(config);
+export const ACCOUNT_MIN_STORAGE_AMOUNT = '0.003'
+export const near = new Near(config)
 
 class SpecialWalletAccount extends ConnectedWalletAccount {
   async sendTransactionWithActions(receiverId: string, actions: Action[]) {
-    return this.signAndSendTransaction(receiverId, actions);
+    return this.signAndSendTransaction(receiverId, actions)
   }
 
   async createTransaction({
     receiverId,
     actions,
-    nonceOffset = 1,
+    nonceOffset = 1
   }: {
-    receiverId: string;
-    actions: Action[];
-    nonceOffset?: number;
+    receiverId: string
+    actions: Action[]
+    nonceOffset?: number
   }) {
     const localKey = await this.connection.signer.getPublicKey(
       this.accountId,
-      this.connection.networkId,
-    );
+      this.connection.networkId
+    )
     const accessKey = await this.accessKeyForTransaction(
       receiverId,
       actions,
-      localKey,
-    );
+      localKey
+    )
     if (!accessKey) {
       throw new Error(
-        `Cannot find matching key for transaction sent to ${receiverId}`,
-      );
+        `Cannot find matching key for transaction sent to ${receiverId}`
+      )
     }
 
-    const block = await this.connection.provider.block({ finality: 'final' });
-    const blockHash = baseDecode(block.header.hash);
+    const block = await this.connection.provider.block({ finality: 'final' })
+    const blockHash = baseDecode(block.header.hash)
 
-    const publicKey = PublicKey.from(accessKey.public_key);
-    const nonce = accessKey.access_key.nonce + nonceOffset;
+    const publicKey = PublicKey.from(accessKey.public_key)
+    const nonce = accessKey.access_key.nonce + nonceOffset
 
     return createTransaction(
       this.accountId,
@@ -68,80 +68,80 @@ class SpecialWalletAccount extends ConnectedWalletAccount {
       receiverId,
       nonce,
       actions,
-      blockHash,
-    );
+      blockHash
+    )
   }
 }
 
 export default class SpecialWallet extends WalletConnection {
-  _connectedAccount!: SpecialWalletAccount;
+  _connectedAccount!: SpecialWalletAccount
 
   account() {
     if (!this._connectedAccount) {
       this._connectedAccount = new SpecialWalletAccount(
         this,
         this._near.connection,
-        this._authData.accountId,
-      );
+        this._authData.accountId
+      )
     }
 
-    return this._connectedAccount;
+    return this._connectedAccount
   }
 
   createTransaction({
     receiverId,
     actions,
-    nonceOffset = 1,
+    nonceOffset = 1
   }: {
-    receiverId: string;
-    actions: Action[];
-    nonceOffset?: number;
+    receiverId: string
+    actions: Action[]
+    nonceOffset?: number
   }) {
     return this._connectedAccount.createTransaction({
       receiverId,
       actions,
-      nonceOffset,
-    });
+      nonceOffset
+    })
   }
 }
-export const wallet = new SpecialWallet(near, null);
+export const wallet = new SpecialWallet(near, null)
 
 export const getWallet = async () => {
-  const near = await connect(config);
+  const near = await connect(config)
 
   // create wallet connection
-  const wallet = new WalletConnection(near, null);
-  return wallet;
-};
+  const wallet = new WalletConnection(near, null)
+  return wallet
+}
 
 export interface AccountStorageView {
-  total: string;
-  available: string;
+  total: string
+  available: string
 }
 
 export const currentStorageBalance = (
-  accountId: string,
+  accountId: string
 ): Promise<AccountStorageView> => {
   return DexViewFunction({
     methodName: 'storage_balance_of',
-    args: { account_id: accountId },
-  });
-};
+    args: { account_id: accountId }
+  })
+}
 
 type StateType = [
   nearAPI.WalletConnection | null,
-  (wallet: nearAPI.WalletConnection) => void,
-];
+  (wallet: nearAPI.WalletConnection) => void
+]
 
-export const NearWalletContext = createContext<StateType>({} as StateType);
+export const NearWalletContext = createContext<StateType>({} as StateType)
 
 export const getGas = (gas?: string) =>
-  gas ? new BN(gas) : new BN('100000000000000');
+  gas ? new BN(gas) : new BN('100000000000000')
 export const getAmount = (amount?: string) =>
-  amount ? new BN(utils.format.parseNearAmount(amount)) : new BN('0');
+  amount ? new BN(utils.format.parseNearAmount(amount)) : new BN('0')
 export const executeMultipleTransactions = async (
   transactions: Transaction[],
-  callbackUrl?: string,
+  callbackUrl?: string
 ) => {
   const nearTransactions = await Promise.all(
     transactions.map((t, i) => {
@@ -153,12 +153,12 @@ export const executeMultipleTransactions = async (
             fc.methodName,
             fc.args,
             getGas(fc.gas),
-            getAmount(fc.amount),
-          ),
-        ),
-      });
-    }),
-  );
+            getAmount(fc.amount)
+          )
+        )
+      })
+    })
+  )
 
-  return wallet.requestSignTransactions(nearTransactions, callbackUrl);
-};
+  return wallet.requestSignTransactions(nearTransactions, callbackUrl)
+}
--- src/near/near.ts
+++ src/near/near.ts
@@ -1,17 +1,16 @@
 export interface NearViewFunctionOptions {
-  methodName: string;
-  args: object;
+  methodName: string
+  args: object
 }
 
 export interface NearFunctionCallOptions extends NearViewFunctionOptions {
-  gas?: string;
-  amount?: string;
+  gas?: string
+  amount?: string
 }
 
 export interface Transaction {
-  receiverId: string;
-  functionCalls: NearFunctionCallOptions[];
+  receiverId: string
+  functionCalls: NearFunctionCallOptions[]
 }
 
-export const DEX_CONTRACT_ID = 'router.kula.testnet';
-export const FARM_CONTRACT_ID = 'form.kula.testnet';
+export const DAO_CONTRACT_ID = 'cryptdao2.kula.testnet'
