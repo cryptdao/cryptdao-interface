3df0b4328c541fdf172aa1655c24034ac303cd27
kula <kula@live.com>
2021-12-28 23:08:44 +0800

    feat: set account login

--- src/components/Header/Account.tsx
+++ src/components/Header/Account.tsx
@@ -1,4 +1,5 @@
 import { wallet } from "@/near/Account";
+import { DAO_CONTRACT_ID } from "@/near/near";
 import { useState } from "react";
 
 export default function Account() {
@@ -16,11 +17,30 @@ export default function Account() {
       {wallet.isSignedIn() ? (
         accountName
       ) : (
-        <button className="px-[24px]">
+        <button
+          className="px-[24px]"
+          onClick={() => {
+            wallet.requestSignIn(DAO_CONTRACT_ID);
+          }}
+        >
           <span className="hidden sm:block">连接钱包</span>
         </button>
       )}
-      <div className={`${hover ? "block" : "hidden"}`}>test</div>
+      <div
+        className={`shell overflow-hidden relative rounded-none md:rounded-lg ${
+          wallet.isSignedIn() && hover ? "block" : "hidden"
+        }`}
+      >
+        <button
+          className="px-[24px] w-full"
+          onClick={() => {
+            wallet.signOut();
+            window.location.assign("/");
+          }}
+        >
+          退出
+        </button>
+      </div>
     </div>
   );
 }
