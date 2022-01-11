98d81fe2610022c0fd9704a843154642aa2e28e2
kula <kula@live.com>
2022-01-11 11:41:02 +0800

    feat: layout config

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,29 +1,28 @@
-import Account from "@/layout/Header/Account";
-import styled from "styled-components";
+import Account from '@/layout/Header/Account'
+import styled from 'styled-components'
 
 export const HeaderHeight = styled.div`
   height: 79px;
-`;
+`
 
 export const HeaderWrapper = styled.div`
   z-index: 20;
   right: 0px;
-`;
+`
 
 export const HeaderCenter = styled.div`
   height: 78px;
-`;
+`
 
 export const HeaderLink = styled.a`
   font-size: 24px;
   padding-top: 4px;
-`;
+`
 
 //TODO - add the following to the Header component:
 export default function Header() {
   return (
     <>
-      <HeaderHeight />
       <HeaderWrapper>
         <nav className="w-full border-b bg">
           <div className="px-4 max-w-[1012px] mx-auto">
@@ -37,5 +36,5 @@ export default function Header() {
         </nav>
       </HeaderWrapper>
     </>
-  );
+  )
 }
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -5,19 +5,16 @@ import Header from './Header'
 export default function Layout(props: { children: React.ReactFragment }) {
   return (
     <>
-      <div className="flex">
-        <aside className="fixed z-40 flex-1 flex-shrink-0 w-[70px] p-3 border-2 h-screen">
-          <Scroller />
-        </aside>
-        <div className="border-2 border-red sm:left-[70px]">
-          <nav className="fixed left-0 w-full h-8 border-2 border-yellow-500">
-            <Header />
-          </nav>
-
-          <main className="border-2 border-green ml-[70px] w-full flex-auto">
-            {props.children}
-          </main>
-        </div>
+      <aside className="fixed z-40  w-[70px]  p-3 border-2 h-screen">
+        <Scroller />
+      </aside>
+      <div className="border-2 border-red wl-[70px]">
+        <nav className="ml-[70px] left-0 w-full h-8 border-2 border-yellow-500 right-0">
+          <Header />
+        </nav>
+        <main className="flex flex-row border-2 border-green ml-[70px] w-full flex-auto">
+          {props.children}
+        </main>
       </div>
     </>
   )
