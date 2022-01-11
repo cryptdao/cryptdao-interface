f153d820116d0b81d4b90401efd416b285dc2530
kula <kula@live.com>
2022-01-11 14:26:16 +0800

    feat: 展示开始结束时间

--- src/pages/ProposalItemPage.tsx
+++ src/pages/ProposalItemPage.tsx
@@ -1,43 +1,63 @@
-import Layout from "@/layout";
-import { daoGetProposal } from "@/near/Function";
-import { Skeleton } from "antd";
-import { id } from "date-fns/locale";
-import { useQuery } from "react-query";
-import { useParams } from "react-router-dom";
+import Layout from '@/layout'
+import { daoGetProposal } from '@/near/Function'
+import { Skeleton } from 'antd'
+import { id } from 'date-fns/locale'
+import { useQuery } from 'react-query'
+import { useParams } from 'react-router-dom'
 
 export default function ProposalItemPage() {
-  const { slug } = useParams();
-  const proposal = useQuery(["proposal", id], () => {
+  const { slug } = useParams()
+  const { isLoading, isError, error, data } = useQuery(['proposal', id], () => {
     if (slug) {
-      const id = Number(slug);
-      return daoGetProposal(id);
+      const id = Number(slug)
+      return daoGetProposal(id)
     } else {
-      return null;
+      return null
     }
-  });
+  })
 
-  if (proposal.isLoading) {
-    return <Skeleton active />;
+  if (isLoading) {
+    return <Skeleton active />
   }
-  if (proposal.isError) {
-    return (
-      <span>
-        Error:{proposal.error instanceof Error ? proposal.error.message : ""}
-      </span>
-    );
+  if (isError) {
+    return <span>Error:{error instanceof Error ? error.message : ''}</span>
   }
 
+  console.log(data)
   return (
     <>
       <Layout>
-        <div className="float-left w-full lg:w-8/12">
+        <div className="flex-1 w-full lg:w-8/12">
           <article className="px-4">
-            <h1>{proposal.data?.title}</h1>
+            <h1>{data?.title}</h1>
+            <p>{data?.description}</p>
           </article>
         </div>
 
-        <div className="w-full">222</div>
+        <div className="flex-1 w-10">
+          <div className="mb-4 border-t border-b">
+            <div className="block px-4 pt-3 border-b">信息</div>
+            <div className="p-4">
+              <div className="flex flex-row">
+                <div className="flex-1 w-1">策略</div>
+                <div className="flex-1 w-1">{data?.kind.type}</div>
+              </div>
+              <div className="flex flex-row">
+                <div className="flex-1 w-1">作者</div>
+                <div className="flex-1 w-1">{data?.proposer}</div>
+              </div>
+              <div className="flex flex-row">
+                <div className="flex-1 w-1">开始时间</div>
+                <div className="flex-1 w-1">{data?.proposal_start_time}</div>
+              </div>
+              <div className="flex flex-row">
+                <div className="flex-1 w-1">结束时间</div>
+                <div className="flex-1 w-1">{data?.proposal_end_time}</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </Layout>
     </>
-  );
+  )
 }
--- src/types.ts
+++ src/types.ts
@@ -1,63 +1,63 @@
 export interface TokenMetadata {
-  id: string;
-  name: string;
-  symbol: string;
-  decimals: number;
-  icon?: string;
-  ref?: number;
-  near?: number;
-  total?: number;
-  amountLabel?: string;
-  amount?: number;
+  id: string
+  name: string
+  symbol: string
+  decimals: number
+  icon?: string
+  ref?: number
+  near?: number
+  total?: number
+  amountLabel?: string
+  amount?: number
 }
 
 export interface DaoMetadata {
-  name: string;
-  headcount: number;
-  last_proposal_id: number;
+  name: string
+  headcount: number
+  last_proposal_id: number
 }
 
 export interface Citizen {
-  account_id: string;
-  role_name: string;
-  joined: number;
+  account_id: string
+  role_name: string
+  joined: number
 }
 
-export interface Kind {}
-
-export interface Kind {}
+export interface Kind {
+  type: string
+}
 
 export interface VoteKind extends Kind {
-  options: VoteOption[];
+  options: VoteOption[]
 }
 
 export enum KindType {
-  VoteKind = "Vote",
+  VoteKind = 'Vote'
 }
 export interface ProposalProps {
-  id: number;
-  proposer: string;
-  title: string;
-  description: string;
-  submission_time: number;
-  proposal_start_time: number;
-  proposal_end_time: number;
-  status: string;
-  kind: Kind;
+  id: number
+  proposer: string
+  title: string
+  description: string
+  submission_time: number
+  proposal_start_time: number
+  proposal_end_time: number
+  status: string
+  kind: Kind
 }
 
 export interface ProposalInputProps {
-  proposer: string;
-  title: string;
-  description: string;
-  proposal_start_time: number;
-  proposal_end_time: number;
-  kind: Kind;
+  proposer: string
+  title: string
+  description: string
+  proposal_start_time: number
+  proposal_end_time: number
+  kind: Kind
 }
 
-export const PAGE_SIZE = 10;
+export const PAGE_SIZE = 10
 
 export interface VoteOption {
-  id: number;
-  value: string;
+  id: number
+  value: string
 }
